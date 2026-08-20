f9558ffa83451b809c9cd59c428b7f7557521e73
nailongooo <nailong2027@gmail.com>
2026-08-21 01:59:02 +0800

    Initialize model container with versioned schema value

--- iSpend/App/iSpendApp.swift
+++ iSpend/App/iSpendApp.swift
@@ -5,7 +5,10 @@ import SwiftData
 struct iSpendApp: App {
     private let container: ModelContainer
     init() {
-        do { container = try ModelContainer(for: iSpendSchemaV1.self, migrationPlan: iSpendMigrationPlan.self) }
+        do {
+            let schema = Schema(versionedSchema: iSpendSchemaV1.self)
+            container = try ModelContainer(for: schema, migrationPlan: iSpendMigrationPlan.self)
+        }
         catch { fatalError("无法创建 iSpend 数据库：\(error.localizedDescription)") }
     }
     var body: some Scene { WindowGroup { RootView() }.modelContainer(container) }
