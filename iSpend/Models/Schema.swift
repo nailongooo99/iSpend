a652ccf820694506e5201157430d3dd0487232fe
nailongooo <nailong2027@gmail.com>
2026-08-21 01:38:25 +0800

    Fix Swift parser and schema concurrency errors

--- iSpend/Models/Schema.swift
+++ iSpend/Models/Schema.swift
@@ -1,7 +1,7 @@
 import SwiftData
 
 enum iSpendSchemaV1: VersionedSchema {
-    static var versionIdentifier = Schema.Version(1, 0, 0)
+    static let versionIdentifier = Schema.Version(1, 0, 0)
     static var models: [any PersistentModel.Type] { [Ledger.self, FinanceCategory.self, Account.self, FinanceTransaction.self, Budget.self, SavingsGoal.self, SavingsRecord.self, RecurringPayment.self, InstallmentPlan.self] }
 }
 
